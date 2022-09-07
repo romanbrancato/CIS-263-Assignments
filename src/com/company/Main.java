48844cd43804d61c7c117fa1e5b3464a049cc4e4
romanbrancato <89923118+romanbrancato@users.noreply.github.com>
2022-09-07 19:20:54 -0400

    coverted to use array lists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,48 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] sortedNumsArray = new int[5];
+        ArrayList<Integer> sortedNumsArray = new ArrayList<>();
+        int maxSize = 5;
         Scanner scanner = new Scanner(System.in);
 
-        //iterate to accept as many inputs as the array can hold
-        for (int i = 0; i < sortedNumsArray.length; i++) {
+        //iterate to accept inputs
+        for (int i = 0; i < maxSize; i++) {
 
             //get number input from user
             System.out.print("Enter Int: ");
             int enteredNum = scanner.nextInt();
-            //add entered number to array
-            sortedNumsArray[i] = enteredNum;
+            //add number to array
+            sortedNumsArray.add(enteredNum);
 
             //Start of Insertion Sort
 
             //begin loop at second element in array
-            for (int j = 1; j < sortedNumsArray.length; j++) {
+            for (int j = 1; j < sortedNumsArray.size(); j++) {
                 //setting the "key" to the second element
-                int key = sortedNumsArray[j];
-                //setting g as the number before the key
-                int g = j - 1;
-                //while g does not go below index 0 and the element before the key is greater than the key
-                while (g > -1 && sortedNumsArray[g] > key) {
-                    //element at index g shift up 1 index to put the lower value before it
-                    sortedNumsArray[g + 1] = sortedNumsArray[g];
-                    //decrement g to check if the same is true in the new position
-                    g = g - 1;
+                int key = sortedNumsArray.get(j);
+                //setting currentPosition as the element before the key
+                int currentPosition = j - 1;
+                //while current position does not go below index 0 and the element before the key is greater than the key
+                while (currentPosition > -1 && sortedNumsArray.get(currentPosition) > key) {
+                    //element at current position shifts up 1 index to put the lower value before it
+                    sortedNumsArray.set(currentPosition + 1, sortedNumsArray.get(currentPosition));
+                    //decrement current position to check if the same is true in the new position
+                    currentPosition = currentPosition - 1;
                 }
-                //set the element after g to the key
-                sortedNumsArray[g + 1] = key;
+                //set the element after current position to the key
+                sortedNumsArray.set(currentPosition + 1, key);
             }
 
             //End of Insertion Sort
 
             //print out sorted array
-            System.out.println(Arrays.toString(sortedNumsArray));
+            System.out.println(sortedNumsArray);
         }
     }
 }
