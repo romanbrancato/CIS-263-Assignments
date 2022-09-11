4502786d00fc31636a9c17a1cfc6aa0ef9f44e34
romanbrancato <89923118+romanbrancato@users.noreply.github.com>
2022-09-11 11:42:34 -0400

    Added Assignment #2

--- src/com/company/Assignment1.java
+++ src/com/company/Assignment1.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Assignment1 {
 
     public static void main(String[] args) {
 
---
+++ src/com/company/Assignment2.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Assignment2 {
+
+    public static void main(String[] args) {
+
+    }
+}
