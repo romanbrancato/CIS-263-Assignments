da6cf1c87fec93bfa6d4170ea60024c4cc85c80b
romanbrancato <89923118+romanbrancato@users.noreply.github.com>
2022-09-18 14:36:00 -0400

    Added Assignment #3

---
+++ src/com/company/Assignment3Part1.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Assignment3Part1 {
+
+    public static void main(String[] args) {
+
+
+    }
+    public void singleLinkedListInsertion{
+
+    }
+    public void doubleLinkedListInsertion{
+
+    }
+
+}
+
---
+++ src/com/company/Assignment3Part2.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Assignment3Part2 {
+
+    public static void main(String[] args) {
+
+    }
+    public void stackPush{
+
+    }
+    public void stackPop{
+
+    }
+
+    public void queueEnqueue{
+
+    }
+    public void queueDequeue{
+
+    }
+}
