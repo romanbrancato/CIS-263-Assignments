61f555fdb2e6b38edbdbab7dc09ef819a6d83c1b
romanbrancato <89923118+romanbrancato@users.noreply.github.com>
2022-09-19 13:44:27 -0400

    Assignment 3 progress

--- src/com/company/Assignment3Part1.java
+++ src/com/company/Assignment3Part1.java
@@ -3,13 +3,20 @@ package com.company;
 class SingleOrDoubleLinkedList {
 
     Node head;
-    Node tail;
 
     static class Node {
 
         int data;
         Node next;
         Node prev;
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
 
         Node(int d) {
             data = d;
@@ -42,10 +49,9 @@ class SingleOrDoubleLinkedList {
         // create node with given data
         Node newNode = new Node(data);
 
-        // if list is empty, new node is head and tail
+        // if list is empty, new node is head
         if (list.head == null) {
             list.head = newNode;
-            list.tail = newNode;
         } else {
             //else go to last node and make it head
             Node last = list.head;
@@ -107,6 +113,68 @@ class SingleOrDoubleLinkedList {
 
         printSingleLinkedList(singleLinkedList);
         printDoubleLinkedList(doubleLinkedList);
+
+        //swapping nodes
+
+        int key1 = 9;
+        int key2 = 11;
+
+        System.out.println("After Swapping " + key1 + " and " + key2);
+
+        ////////////////////////////////////////FOR SINGLE LINKED LIST
+        Node singleCurrNode1 = singleLinkedList.head;
+        Node singlePrev1 = null;
+        Node singleCurrNode2 = singleLinkedList.head;
+        Node singlePrev2 = null;
+        //searching for node with key1
+        while(singleCurrNode1.data != key1 && singleCurrNode1 != null){
+            singlePrev1 = singleCurrNode1;
+            singleCurrNode1 = singleCurrNode1.next;
+        }
+        //searching for node with key2
+        while(singleCurrNode2.data != key2 && singleCurrNode2 != null){
+            singlePrev2 = singleCurrNode2;
+            singleCurrNode2 = singleCurrNode2.next;
+        }
+        //swapping next pointers of nodes
+        singlePrev1.next = singleCurrNode2;
+        singlePrev2.next = singleCurrNode1;
+        Node singleTemp = singleCurrNode1.next;
+        singleCurrNode1.next = singleCurrNode2.next;
+        singleCurrNode2.next=singleTemp;
+
+        printSingleLinkedList(singleLinkedList);
+
+        ////////////////////////////////////////FOR DOUBLE LINKED LIST
+
+        Node doubleCurrNode1 = doubleLinkedList.head;
+        Node doublePrev1 = null;
+        Node doubleCurrNode2 = doubleLinkedList.head;
+        Node doublePrev2 = null;
+        //searching for node with key1
+        while(doubleCurrNode1.data != key1 && doubleCurrNode1 != null){
+            doublePrev1 = doubleCurrNode1;
+            doubleCurrNode1 = doubleCurrNode1.next;
+        }
+        //searching for node with key2
+        while(doubleCurrNode2.data != key2 && doubleCurrNode2 != null){
+            doublePrev2 = doubleCurrNode2;
+            doubleCurrNode2 = doubleCurrNode2.next;
+        }
+        //swapping pointers of nodes
+        doublePrev1.next = doubleCurrNode2;
+        doublePrev2.next = doubleCurrNode1;
+        Node doubleTemp = doubleCurrNode1.next;
+        doubleCurrNode1.next = doubleCurrNode2.next;
+        doubleCurrNode2.next=doubleTemp;
+
+        printDoubleLinkedList(doubleLinkedList);
+
+
+
+
+
+
 
     }
 }
